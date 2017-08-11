6dffa72b661b4ae5c44efbf429e913ead089fd0d
vishaljasrotia <vishal.singh8989@gmail.com>
2017-08-11 23:59:40 +0530

    Drag and rearrage tabs working.

--- app/src/main/java/com/kepler/news/newsly/IntroFragments/NewsSourceFragment.java
+++ app/src/main/java/com/kepler/news/newsly/IntroFragments/NewsSourceFragment.java
@@ -180,7 +180,32 @@ public class NewsSourceFragment extends Fragment{
             }
         });
 
+        countryListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
 
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
 
         return v;
     }
